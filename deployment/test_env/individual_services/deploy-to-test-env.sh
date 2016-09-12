ab1f0898c1f51750e31e137946899e281c5d617d
pfcui <cuipengfei2008@gmail.com>
2016-09-12 18:33:44 +0800

    shell script for deployment

--- deployment/test_env/individual_services/deploy-to-test-env.sh
+++ deployment/test_env/individual_services/deploy-to-test-env.sh
@@ -2,4 +2,6 @@
 export DOCKER_TLS_VERIFY="1"
 export DOCKER_HOST="tcp://elb-test-env-swarm-683069932.us-east-1.elb.amazonaws.com:2376"
 
+curl -LO https://raw.githubusercontent.com/OpenLMIS/openlmis-config/master/.env
+
 /usr/local/bin/docker-compose -f $1 up -d
