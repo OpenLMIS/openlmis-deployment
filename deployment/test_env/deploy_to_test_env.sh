6c3089405a2bc4c67e8dda9930e4a06e04b998ae
pfcui <cuipengfei2008@gmail.com>
2016-09-15 15:01:58 +0800

    control start up order

---
+++ deployment/test_env/deploy_to_test_env.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+export DOCKER_TLS_VERIFY="1"
+export DOCKER_HOST="tcp://elb-test-env-swarm-683069932.us-east-1.elb.amazonaws.com:2376"
+
+curl -LO https://raw.githubusercontent.com/OpenLMIS/openlmis-config/master/.env
+
+/usr/local/bin/docker-compose down
+
+/usr/local/bin/docker-compose up -d
+
+/usr/local/bin/docker-compose logs
