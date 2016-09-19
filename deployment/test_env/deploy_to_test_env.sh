90fe857752aa09f9579205c3c0563b218bd6d7a2
pfcui <cuipengfei2008@gmail.com>
2016-09-19 13:39:22 +0800

    don't use docker-machine in jenkins

--- deployment/test_env/deploy_to_test_env.sh
+++ deployment/test_env/deploy_to_test_env.sh
@@ -1,12 +1,7 @@
 #!/usr/bin/env bash
 
-/usr/local/bin/docker-machine scp ./nginx.tmpl host1:~/nginx.tmpl
-
-eval $(/usr/local/bin/docker-machine env host1)
-
-# the above two commands can only run after the cert files are properly located in jenkins
-# details of how to do that is described in the provision markdown
-
+export DOCKER_TLS_VERIFY="1"
+export DOCKER_HOST="tcp://elb-test-env-swarm-683069932.us-east-1.elb.amazonaws.com:2376"
 
 curl -LO https://raw.githubusercontent.com/OpenLMIS/openlmis-config/master/.env
 
