fec97245473abcd665e43eebeb01b59702e386d2
pfcui <cuipengfei2008@gmail.com>
2016-09-19 15:58:29 +0800

    options to keep or wipe data

--- deployment/test_env/deploy_to_test_env.sh
+++ deployment/test_env/deploy_to_test_env.sh
@@ -13,6 +13,15 @@ docker pull jwilder/nginx-proxy
 
 docker pull $1
 
+if [ $KEEP_OR_WIPE == "wipe" ];
+#when run locally, and the env var is not present, else branch will run, thus to keep data
+then
+echo "will WIPE data!!"
+/usr/local/bin/docker-compose down -v
+else
+echo "will keep data";
 /usr/local/bin/docker-compose down
+fi
+
 
 /usr/local/bin/docker-compose up -d
