533e112206023b3969f324e9798c0f327923bd3f
pfcui <cuipengfei2008@gmail.com>
2016-09-21 15:26:32 +0800

    script to handle monitor set up

---
+++ monitoring/start_monitor.sh
@@ -0,0 +1,9 @@
+curl -sL https://github.com/OpenLMIS/openlmis-deployment/archive/master.tar.gz | tar xz
+
+cd /openlmis-deployment-master/monitoring
+
+curl -L https://github.com/docker/compose/releases/download/1.8.0/docker-compose-`uname -s`-`uname -m` > docker-compose
+
+chmod +x docker-compose
+
+docker-compose up -d
