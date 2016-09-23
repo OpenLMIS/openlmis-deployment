9766756800b38554f693bc811a868475c16f5ef5
pfcui <cuipengfei2008@gmail.com>
2016-09-23 14:35:22 +0800

    use copied env file, not downloaded one

--- deployment/test_env/deploy_to_test_env.sh
+++ deployment/test_env/deploy_to_test_env.sh
@@ -2,7 +2,7 @@
 
 export DOCKER_TLS_VERIFY="1"
 export DOCKER_HOST="tcp://elb-test-env-swarm-683069932.us-east-1.elb.amazonaws.com:2376"
-export DOCKER_CERT_PATH="${PWD%/*/*}/TestHost"
+export DOCKER_CERT_PATH="${PWD%/*/*}"
 
 ../shared/init_env.sh
 
