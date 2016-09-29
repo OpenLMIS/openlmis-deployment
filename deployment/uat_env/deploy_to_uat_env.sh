377ddcb836cf95c3a3896545da982142157a773c
pfcui <cuipengfei2008@gmail.com>
2016-09-29 18:15:23 +0800

    use elb for uat env

--- deployment/uat_env/deploy_to_uat_env.sh
+++ deployment/uat_env/deploy_to_uat_env.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 export DOCKER_TLS_VERIFY="1"
-export DOCKER_HOST="tcp://52.52.3.5:2376"
+export DOCKER_HOST="tcp://uat-env-elb-1284865908.us-east-1.elb.amazonaws.com:2376"
 export DOCKER_CERT_PATH="${PWD%/*/*}"
 
 ../shared/init_env.sh
