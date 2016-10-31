c77a4972ba8c9fc700587ca67988791bab5824cf
pfcui <cuipengfei2008@gmail.com>
2016-10-31 11:02:28 +0800

    OLMIS-1226 copy env file from the fixed relative path

--- deployment/shared/init_env.sh
+++ deployment/shared/init_env.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
 cp -r $credentials ./credentials
+cp ./credentials/.env ./.env
 # Jenkins will inject an env var called "credentials"
 # which points to a randomly generated path that contains the keys
