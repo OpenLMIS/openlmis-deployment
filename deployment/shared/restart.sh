f2a41078b9ba2d5de2d3de13feea9473b90dc225
Pawel Nawrocki <pnawrocki@soldevelo.com>
2016-12-05 21:52:06 +0100

    OLMIS-982: Preserve the KEEP_OR_WIPE setting for single container restart

--- deployment/shared/restart.sh
+++ deployment/shared/restart.sh
@@ -1,17 +1,28 @@
 #!/usr/bin/env bash
 
+$WIPE_MSG="Will WIPE data!"
+$KEEP_MSG="Will keep data."
+
 if [ ! -z "$1" ];
   then
-  /usr/local/bin/docker-compose stop $1
-  /usr/local/bin/docker-compose up -d $1
+  if [ "$KEEP_OR_WIPE" == "wipe" ];
+    #when run locally, and the env var is not present, else branch will run, thus to keep data
+    then
+    echo "$WIPE_MSG"
+    /usr/local/bin/docker-compose rm -fv $1
+  else
+    echo "$KEEP_MSG";
+    /usr/local/bin/docker-compose stop $1
+    /usr/local/bin/docker-compose up -d $1
+  fi
 else
   if [ "$KEEP_OR_WIPE" == "wipe" ];
     #when run locally, and the env var is not present, else branch will run, thus to keep data
     then
-    echo "Will WIPE data!"
+    echo "$WIPE_MSG"
     /usr/local/bin/docker-compose down -v
   else
-    echo "Will keep data";
+    echo "$KEEP_MSG";
     /usr/local/bin/docker-compose down
   fi
   /usr/local/bin/docker-compose up -d
