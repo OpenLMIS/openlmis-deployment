9433ef8e88a96cc9932fe29bbb817d0eb02389cb
joshzamor <josh.zamor@villagereach.org>
2017-02-08 17:30:49 -0800

    OLMIS-1766, add build and recreate option
    this option is needed now that test has adopted Blue’s config image

--- deployment/shared/restart.sh
+++ deployment/shared/restart.sh
@@ -8,9 +8,9 @@ KEEP_MSG="Will keep data."
 if [ "$KEEP_OR_WIPE" == "wipe" ]; then
   echo "$WIPE_MSG"
   unset spring_profiles_active
-  /usr/local/bin/docker-compose up -d
+  /usr/local/bin/docker-compose up --build --force-recreate -d
 else
   echo "$KEEP_MSG";
   export spring_profiles_active="production"
-  /usr/local/bin/docker-compose up -d
+  /usr/local/bin/docker-compose up --build --force-recreate -d
 fi
