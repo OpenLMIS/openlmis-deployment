10137c5906ad62ecb817406ebe151d86c38bcc14
Josh Zamor <josh.zamor@villagereach.org>
2017-08-07 11:42:07 -0700

    OLMIS-2957, fix restart script pull from compose
    pull from the compose based on service name keeps with defining the versions within that deployment descriptor

--- deployment/shared/pull_images.sh
+++ deployment/shared/pull_images.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env bash
 
-docker pull openlmis/rsyslog:1
-docker pull openlmis/postgres:9.6-postgis
-docker pull openlmis/nginx:1
-docker pull gliderlabs/consul-server
+/usr/local/bin/docker-compose pull log
+/usr/local/bin/docker-compose pull db
+/usr/local/bin/docker-compose pull nginx
+/usr/local/bin/docker-compose pull consul
 
 # $1 is the parameter passed in
 /usr/local/bin/docker-compose pull $1
-
