d5153464b44a7c6b2b0645d70ff67aab8d4cbdb7
Chongsun Ahn <cahn922@yahoo.com>
2017-08-23 14:34:28 -0700

    Create new performance test deployment section
    This new section is almost the same as test_env, except for the shell script, which points to the performance test server, and the docker-compose YAML file, which adds the performance-data Spring profile to Requisition.

---
+++ deployment/perftest_env/deploy_to_perftest_env.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+export DOCKER_TLS_VERIFY="1"
+export DOCKER_HOST="tcp://perftest-env-elb-2066004734.us-east-1.elb.amazonaws.com:2376"
+export DOCKER_CERT_PATH="${PWD}/credentials"
+
+../shared/init_env.sh
+
+../shared/pull_images.sh $1
+
+../shared/restart.sh $1
