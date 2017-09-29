1d2637a564583277f36cad30deae4ac5e991d187
Chongsun Ahn <cahn922@yahoo.com>
2017-09-29 13:35:58 -0700

    OLMIS-3269 Fix some issues with deployment configuration
    Our scripts do not use the env file much for deployment; we set them in three different places. This causes perftest deployment to load demo-data and performance-data profiles, even though we are now using a snapshot database for performance testing. These changes allow perftest the option to use whatever is in the env file (which only had production profile set), without adding the unnecessary demo-data and performance-data profiles.
    
    We might improve this again by only setting demo-data, performance-data profiles in each specific environment and let the env file handle most other profiles.

--- deployment/shared/restart.sh
+++ deployment/shared/restart.sh
@@ -2,6 +2,7 @@
 
 WIPE_MSG="Will WIPE data!"
 KEEP_MSG="Will keep data."
+USE_ENV_MSG="Will use whatever is in the env file."
 
 /usr/local/bin/docker-compose kill
 /usr/local/bin/docker-compose down
@@ -10,8 +11,11 @@ if [ "$KEEP_OR_WIPE" == "wipe" ]; then
   echo "$WIPE_MSG"
   export spring_profiles_active="demo-data,refresh-db"
   /usr/local/bin/docker-compose up --build --force-recreate -d
-else
+else if [ "$KEEP_OR_WIPE" == "keep" ]; then
   echo "$KEEP_MSG";
   export spring_profiles_active="production,demo-data"
+  /usr/local/bin/docker-compose up --build --force-recreate -d
+else
+  echo "$USE_ENV_MSG";
   /usr/local/bin/docker-compose up --build --force-recreate -d
 fi
