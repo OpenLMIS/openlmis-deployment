00d1d9b8051eb7c4e526569ea11610bbeda86caf
Chongsun Ahn <cahn922@yahoo.com>
2017-10-19 18:09:32 -0700

    Revert "OLMIS-3269, update restart script to better set profile"
    This reverts commit 33a28d463f371df2e4d7923fd5e7ae1d27e80a54.

--- deployment/shared/restart.sh
+++ deployment/shared/restart.sh
@@ -1,35 +1,21 @@
 #!/usr/bin/env bash
 
-: "${KEEP_OR_WIPE:?Need to set KEEP_OR_WIPE}"
-
 WIPE_MSG="Will WIPE data!"
 KEEP_MSG="Will keep data."
 USE_ENV_MSG="Will use whatever is in the env file."
 
-# Bring it down
 /usr/local/bin/docker-compose kill
 /usr/local/bin/docker-compose down -v
 
-# source the env file as we want to know the spring profiles
-source .env
-: "${spring_profiles_active:?Need to set spring_profiles_active}"
-
-# based on KEEP_OR_WIPE we do/don't change the profiles set
-PROFILES=$spring_profiles_active
-echo "Profiles read from env: $PROFILES"
 if [ "$KEEP_OR_WIPE" == "wipe" ]; then
   echo "$WIPE_MSG"
-  PROFILES="${PROFILES//production}"
+  export spring_profiles_active="demo-data,refresh-db"
+  /usr/local/bin/docker-compose up --build --force-recreate -d
 elif [ "$KEEP_OR_WIPE" == "keep" ]; then
   echo "$KEEP_MSG"
-  if [[ $PROFILES != *"production"* ]]; then
-    PROFILES="$PROFILES,production"
-  fi
+  export spring_profiles_active="production,demo-data"
+  /usr/local/bin/docker-compose up --build --force-recreate -d
 else
   echo "$USE_ENV_MSG"
+  /usr/local/bin/docker-compose up --build --force-recreate -d
 fi
-export spring_profiles_active=$PROFILES
-echo "Profiles to use: $spring_profiles_active"
-
-# start it up
-/usr/local/bin/docker-compose up --build --force-recreate -d
