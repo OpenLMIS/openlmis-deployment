7e711de1fd9951c0ca17e44d82b73afdc1833476
joshzamor <josh.zamor@villagereach.org>
2017-10-19 21:41:27 -0700

    OLMIS-3269, move test to be GH env based.

--- deployment/test_env/deploy_to_test_env.sh
+++ deployment/test_env/deploy_to_test_env.sh
@@ -4,7 +4,7 @@ export DOCKER_TLS_VERIFY="1"
 export DOCKER_HOST="tcp://elb-test-env-swarm-683069932.us-east-1.elb.amazonaws.com:2376"
 export DOCKER_CERT_PATH="${PWD}/credentials"
 
-../shared/init_env.sh
+../shared/init_env_gh.sh
 
 ../shared/pull_images.sh $1
 
