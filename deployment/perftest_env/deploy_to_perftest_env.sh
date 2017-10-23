2d20b79085ef630ef66c61ccb4f46ffbaee40766
joshzamor <josh.zamor@villagereach.org>
2017-10-23 16:34:38 -0700

    olmis-3269, pull latest images first

--- deployment/perftest_env/deploy_to_perftest_env.sh
+++ deployment/perftest_env/deploy_to_perftest_env.sh
@@ -9,6 +9,8 @@ export DOCKER_CERT_PATH="${PWD}/credentials"
 if [ ! -z "$ENV_RESTORE_SNAPSHOT" ]; then
   export KEEP_OR_WIPE="use_env"
   cp .deployment-config/$ENV_RESTORE_SNAPSHOT .env
+  docker pull openlmis/restore-snapshot
+  docker pull openlmis/obscure-data
   /usr/bin/docker run --rm --env-file .env openlmis/restore-snapshot
   /usr/bin/docker run --rm --env-file .env openlmis/obscure-data
 else
