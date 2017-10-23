b43f79673b4f0ea7cad323de909fa470941d31f7
joshzamor <josh.zamor@villagereach.org>
2017-10-23 13:07:22 -0700

    OLMIS-3269, add snapshot image loading to perftest

--- deployment/perftest_env/deploy_to_perftest_env.sh
+++ deployment/perftest_env/deploy_to_perftest_env.sh
@@ -6,6 +6,15 @@ export DOCKER_CERT_PATH="${PWD}/credentials"
 
 ../shared/init_env_gh.sh
 
+if [ ! -z "$ENV_RESTORE_SNAPSHOT" ]; then
+  export KEEP_OR_WIPE="use_env"
+  cp .deployment-config/$ENV_RESTORE_SNAPSHOT .env
+  docker-compose --rm openlmis/restore-snapshot
+  docker-compose --rm openlmis/obscure-data
+else
+  cp .deployment-config/perftest.env .env
+fi
+
 ../shared/pull_images.sh $1
 
 ../shared/restart.sh $1
