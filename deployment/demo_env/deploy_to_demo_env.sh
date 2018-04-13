1cf3a985e62d29d53f99765011e6f65052e8556f
Josh Zamor <josh.zamor@villagereach.org>
2018-04-12 23:24:21 -0400

    olmis-4474, update demo-v3 docker host address
    Host address changed as AWS ec2 instance was retired, and we used the opportunity to consolidate the VPCs.

--- deployment/demo_env/deploy_to_demo_env.sh
+++ deployment/demo_env/deploy_to_demo_env.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 export DOCKER_TLS_VERIFY="1"
-export DOCKER_HOST="v3-demo-env-elb-2037688312.us-east-1.elb.amazonaws.com:2376"
+export DOCKER_HOST="demo-v3.openlmis.org:2376"
 export DOCKER_CERT_PATH="${PWD}/credentials"
 
 ../shared/init_env_gh.sh
