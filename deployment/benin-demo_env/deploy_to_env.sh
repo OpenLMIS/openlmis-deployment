69c982b8e6bbc59a74bc5b24909b383875f2921e
Wes <wes@weslandia.org>
2018-08-24 14:18:45 -0400

    Update deploy_to_env.sh
    Added section to restore an RDS snapshot, if specified

--- deployment/benin-demo_env/deploy_to_env.sh
+++ deployment/benin-demo_env/deploy_to_env.sh
@@ -6,6 +6,17 @@ export DOCKER_CERT_PATH="${PWD}/credentials"
 
 ../shared/init_env_gh.sh
 
+if [ ! -z "$ENV_RESTORE_SNAPSHOT" ]; then
+  export KEEP_OR_WIPE="use_env"
+  cp .deployment-config/$ENV_RESTORE_SNAPSHOT settings.env
+  docker pull openlmis/restore-snapshot
+  docker pull openlmis/obscure-data
+  /usr/bin/docker run --rm --env-file settings.env openlmis/restore-snapshot
+  sleep 600
+else
+  cp .deployment-config/benin-demo.env settings.env
+fi
+
 ../shared/pull_images.sh $1
 
 ../shared/restart.sh $1
