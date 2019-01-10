75b6f82cb3cdf5c99700126e5415d85953587937
Mateusz Kwiatkowski <mkwiatkowski@soldevelo.com>
2019-01-10 11:18:15 +0100

    OLMIS-5728: added deployment files for functional test server

---
+++ deployment/functional-test/deploy_to_env.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+export DOCKER_TLS_VERIFY="1"
+export DOCKER_HOST="tcp://functional-test.openlmis.org:2376"
+export DOCKER_CERT_PATH="${PWD}/credentials"
+
+../shared/init_env_gh.sh
+
+../shared/pull_images.sh $1
+
+../shared/restart.sh $1
