6e0e8440f3daf2ccc90f60d2b698098dd0c1b1e4
Mateusz Kwiatkowski <mkwiatkowski@soldevelo.com>
2019-01-16 13:19:02 +0100

    OLMIS-5728: rearanged deploy script for deploying functional test instance
    trying to get rid of problem with docker host

--- deployment/functional_test_env/deploy_to_env.sh
+++ deployment/functional_test_env/deploy_to_env.sh
@@ -2,16 +2,17 @@
 
 docker pull openlmis/start-instance
 
+if $START_EC2_INSTANCE; then
+  echo "Starting EC2 instance for functional test server"
+  /usr/bin/docker run --rm --env-file settings.env openlmis/start-instance
+fi
+
 export DOCKER_TLS_VERIFY="1"
 export DOCKER_HOST="tcp://functional-test.openlmis.org:2376"
 export DOCKER_CERT_PATH="${PWD}/credentials"
 
 ../shared/init_env_gh.sh
 
-if $START_EC2_INSTANCE; then
-  echo "Starting EC2 instance for functional test server"
-  /usr/bin/docker run --rm --env-file settings.env openlmis/start-instance
-fi
 
 ../shared/pull_images.sh $1
 
