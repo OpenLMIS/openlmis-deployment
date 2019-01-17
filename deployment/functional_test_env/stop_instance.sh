a395373baf86ee75ecdbac906c1e4c0e6efba58b
Mateusz Kwiatkowski <mkwiatkowski@soldevelo.com>
2019-01-17 11:20:12 +0100

    Revert "OLMIS-5728: added stop instance script for functional test"
    This reverts commit b0116962a058d64e5a07849c3a2548b4d3efe52a.

--- deployment/functional_test_env/stop_instance.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-docker pull openlmis/stop-instance
-echo "Stopping EC2 instance for functional test server"
-/usr/bin/docker run --rm --env-file settings.env openlmis/stop-instance
-
