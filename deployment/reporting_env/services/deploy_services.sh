4a851ad5af797ed012be238a83c64898dbe36333
Morris Mukiri <morrismukiri@gmail.com>
2019-04-10 13:34:37 +0300

    update docker certificates to use
    Signed-off-by: Morris Mukiri <morrismukiri@gmail.com>

--- deployment/reporting_env/services/deploy_services.sh
+++ deployment/reporting_env/services/deploy_services.sh
@@ -3,7 +3,6 @@
 export DOCKER_TLS_VERIFY="1"
 export COMPOSE_TLS_VERSION=TLSv1_2
 export DOCKER_HOST="tcp://nifi-registry.openlmis.org:2376"
-# export DOCKER_CERT_PATH="${PWD}/credentials"
 export DOCKER_COMPOSE_BIN=/usr/local/bin/docker-compose
 export REPORTING_DIR_NAME=reporting
 
@@ -18,8 +17,6 @@ export NIFI_DOMAIN_NAME=nifi.uat.openlmis.org
 
 reportingRepo=$1
 
-# cp -r "$docker_credentials" "$DOCKER_CERT_PATH"
-# cp -r "$reporting_ssl" "$reportingRepo/$REPORTING_DIR_NAME/config/services/nginx/tls/reporting-uat"
 cd "$reportingRepo/$REPORTING_DIR_NAME"
 $DOCKER_COMPOSE_BIN kill
 $DOCKER_COMPOSE_BIN down -v
