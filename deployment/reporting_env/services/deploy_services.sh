260f8bb983d2329009ad06b685fbc0e86ce47f25
Morris Mukiri <morrismukiri@gmail.com>
2019-04-10 11:39:24 +0300

    Set TLS version to use with docker
    Signed-off-by: Morris Mukiri <morrismukiri@gmail.com>

--- deployment/reporting_env/services/deploy_services.sh
+++ deployment/reporting_env/services/deploy_services.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 export DOCKER_TLS_VERIFY="1"
+export COMPOSE_TLS_VERSION=TLSv1_2
 export DOCKER_HOST="tcp://nifi-registry.openlmis.org:2376"
 export DOCKER_CERT_PATH="${PWD}/credentials"
 export DOCKER_COMPOSE_BIN=/usr/local/bin/docker-compose
