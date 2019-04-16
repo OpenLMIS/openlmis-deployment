d2316436e282f281db69c192cc576fd36bd95331
joshzamor <josh.zamor@villagereach.org>
2019-04-16 11:40:28 -0700

    OLMIS-6192, add uat2 to terraform

---
+++ provision/terraform/uat/uat2/main.tf
@@ -0,0 +1,24 @@
+terraform {
+  backend "s3" {
+    "bucket" = "openlmis-terraform-states"
+    "key"    = "uat-uat2.tf"
+    "region" = "us-east-1"
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+module "uat2" {
+  source = "../../modules/openlmis"
+
+  name                         = "${var.name}"
+  app-instance-ssh-user        = "${var.app-instance-ssh-user}"
+  docker-ansible-dir           = "${var.docker-ansible-dir}"
+  docker-tls-port              = "${var.docker-tls-port}"
+  app-tls-s3-access-key-id     = "${var.aws_access_key_id}"
+  app-tls-s3-secret-access-key = "${var.aws_secret_access_key}"
+  app-dns-name                 = "${var.app-dns-name}"
+  app-instance-group           = "${var.app-instance-group}"
+}
---
+++ provision/terraform/uat/uat2/terraform.tfvars
@@ -0,0 +1,11 @@
+name = "uat2"
+
+app-instance-ssh-user = "ubuntu"
+
+docker-ansible-dir = "../../../ansible/"
+
+docker-tls-port = "2376"
+
+app-dns-name = "uat2.openlmis.org"
+
+app-instance-group = "docker-hosts"
