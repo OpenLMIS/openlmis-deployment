8bdb9eb972ecbe71bfbd14362561725700ee1658
Jason Rogena <jason@rogena.me>
2019-04-24 11:42:12 +0300

    fixup! OLMIS-6229 Add S3 backup container

--- deployment/reporting_env/nifi-registry/s3-backup/sync-s3.sh
+++ deployment/reporting_env/nifi-registry/s3-backup/sync-s3.sh
@@ -1,6 +1,8 @@
-
 #!/bin/sh
+
+# Immediately exit if any of the command return codes is anything other than a 0
 set -e
+
 # Download the remote files first
 aws s3 cp --no-progress --recursive $S3_BACKUP_URI $BACKUP_DIRECTORY
 
