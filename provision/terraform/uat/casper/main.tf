82b9957808324aee713cf9d6d3860f46191d1ba4
pgarrison <philipmg@cs.washington.edu>
2019-06-24 12:23:09 -0700

    Make terraform environment for casper
    Not complete! The main missing piece is to configure Route 53 to get us
    the casper.a.openlmis.org domain.

---
+++ provision/terraform/uat/casper/main.tf
@@ -0,0 +1,27 @@
+terraform {
+  backend "s3" {
+    "bucket" = "openlmis-terraform-states"
+    "key"    = "casper.tf"
+    "region" = "us-east-1"
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+module "casper" {
+  source = "../../modules/openlmis"
+
+  name                         = "${var.name}"
+  app-instance-ssh-user        = "${var.app-instance-ssh-user}"
+  docker-ansible-dir           = "${var.docker-ansible-dir}"
+  docker-tls-port              = "${var.docker-tls-port}"
+  app-tls-s3-access-key-id     = "${var.aws_access_key_id}"
+  app-tls-s3-secret-access-key = "${var.aws_secret_access_key}"
+  app-dns-name                 = "${var.app-dns-name}"
+  app-instance-group           = "${var.app-instance-group}"
+  olmis-db-instance-class      = "${var.olmis-db-instance-class}"
+  olmis-db-username            = "${var.olmis-db-username}"
+  olmis-db-password            = "${var.olmis-db-password}"
+}
---
+++ provision/terraform/uat/casper/terraform.tfvars
@@ -0,0 +1,15 @@
+name = "casper"
+
+app-instance-ssh-user = "ubuntu"
+
+docker-ansible-dir = "../../../ansible/"
+
+docker-tls-port = "2376"
+
+app-dns-name = "casper.a.openlmis.org"
+
+app-instance-group = "docker-hosts"
+
+olmis-db-instance-class = "db.t2.medium"
+olmis-db-username="pHvNEhePnLpdATZe"
+olmis-db-password="xkRFqrFD6qSq7wjH"
