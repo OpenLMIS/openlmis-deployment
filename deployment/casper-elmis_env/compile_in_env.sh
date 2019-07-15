aefd3a870ddd2b06583e13be7cda6e9352b95615
pgarrison <philipmg@cs.washington.edu>
2019-07-15 16:28:28 -0700

    OLMIS-6425 Separate CI jobs for compile/deploy
    In openlmis/casper-elmis:0c7bfd8, we added a separate image
    `elmis_builder` so that we can make runtime config changes without
    needing to recompile. This finishes that up by adding a second CI job.

--- deployment/casper-elmis_env/compile_in_env.sh
+++ deployment/casper-elmis_env/compile_in_env.sh
@@ -8,6 +8,4 @@ export DOCKER_CERT_PATH="${PWD}/credentials"
 cp -r $credentials ./credentials
 cd ./build
 
-/usr/local/bin/docker-compose down -v
-/usr/local/bin/docker-compose -f docker-compose.builder.yml build image
-/usr/local/bin/docker-compose up -d
+/usr/local/bin/docker-compose -f docker-compose.builder.yml build baseimage buildimage
