34cf8a491f0dcae8c468a8a8c877c6e4cbdb6f68
pgarrison <philipmg@cs.washington.edu>
2019-08-08 17:14:02 -0700

    OLMIS-6496 Provision casper NiFi registry

---
+++ provision/terraform/casper/nifi/main.tf
@@ -0,0 +1,41 @@
+terraform {
+  backend "s3" {
+    "bucket" = "openlmis-terraform-states"
+    "key"    = "casper-nifi-registry.tf"
+    "region" = "us-east-1"
+  }
+}
+
+provider "aws" {
+  region = "${var.aws-region}"
+}
+
+module "casper-nifi-registry" {
+  source = "../../modules/nifi-registry"
+
+  nr-instance-type            = "${var.nr-instance-type}"
+  nr-volume-size              = "${var.nr-volume-size}"
+  nr-ssh-key                  = "${var.nr-ssh-key}"
+  nr-name                     = "${var.nr-name}"
+  env                         = "${var.env}"
+  bill-to                     = "${var.bill-to}"
+  nr-http-port                = "${var.nr-http-port}"
+  nr-https-port               = "${var.nr-https-port}"
+  nr-subnet-id                = "${var.nr-subnet-id}"
+  nr-ami                      = "${var.nr-ami}"
+  nr-vpc-name                 = "${var.nr-vpc-name}"
+  nr-instance-group           = "${var.nr-instance-group}"
+  nr-instance-ssh-user        = "${var.nr-instance-ssh-user}"
+  docker-ansible-dir          = "${var.docker-ansible-dir}"
+  nr-dns-name                 = "${var.nr-dns-name}"
+  nr-tls-s3-access-key-id     = "${var.aws_access_key_id}"
+  nr-tls-s3-secret-access-key = "${var.aws_secret_access_key}"
+  docker-tls-port             = "${var.docker-tls-port}"
+  nr-assign-elastic-ip        = "${var.nr-assign-elastic-ip}"
+  nr-aws-tls-cert-domain      = "${var.nr-aws-tls-cert-domain}"
+  nr-elb-subnets              = "${var.nr-elb-subnets}"
+  nr-use-route53-domain       = "${var.nr-use-route53-domain}"
+  nr-route53-zone-name        = "${var.nr-route53-zone-name}"
+  nr-nifi-domain              = "${var.nr-nifi-domain}"
+  nr-superset-domain          = "${var.nr-superset-domain}"
+}
---
+++ provision/terraform/casper/nifi/variables.tf
@@ -0,0 +1,133 @@
+variable "nr-instance-type" {
+  type        = "string"
+  description = "AWS instance type for the NiFi Registry host"
+}
+
+variable "nr-volume-size" {
+  type        = "string"
+  description = "The size in GB of the root block device fro the NiFi Registry host"
+}
+
+variable "nr-ssh-key" {
+  type        = "string"
+  description = "The SSH public key to copy over to the NiFi Registry host"
+}
+
+variable "nr-name" {
+  type        = "string"
+  description = "The name tag to give the NiFi Registry host"
+}
+
+variable "env" {
+  type        = "string"
+  description = "The environment the set of AWS resources are being provisioned for"
+}
+
+variable "bill-to" {
+  type        = "string"
+  description = "Which project to bill the provisioned resources"
+}
+
+variable "nr-http-port" {
+  type        = "string"
+  description = "The HTTP port NiFi Registry runs on"
+}
+
+variable "nr-https-port" {
+  type        = "string"
+  description = "The HTTPS port NiFi Registry runs on"
+}
+
+variable "nr-subnet-id" {
+  type        = "string"
+  description = "ID of sububnet the NiFi Registry host should be in"
+}
+
+variable "nr-ami" {
+  type        = "string"
+  description = "The AMI to use with the NiFi Registry instance"
+}
+
+variable "nr-vpc-name" {
+  type        = "string"
+  description = "The name of the VPC to place NiFi Registry resources on AWS"
+}
+
+variable "nr-instance-group" {
+  type        = "string"
+  description = "The deployment group to place the NiFi Registry host. Will dictate what Ansible playbooks can ran on it"
+}
+
+variable "nr-instance-ssh-user" {
+  type        = "string"
+  description = "The name of the user to connect as in the NiFi Registry host"
+}
+
+variable "docker-ansible-dir" {
+  type        = "string"
+  description = "The path to the directory containing the playbook for installing docker"
+}
+
+variable "nr-dns-name" {
+  type        = "string"
+  description = "The DNS name associated to NiFi Registry"
+}
+
+variable "aws_access_key_id" {
+  type        = "string"
+  description = "The AWS access key ID to use to backup generated Docker TLS files"
+}
+
+variable "aws_secret_access_key" {
+  type        = "string"
+  description = "The AWS secrect access key to use to backup generated Docker TLS files"
+}
+
+variable "docker-tls-port" {
+  type        = "string"
+  description = "The TCP port The Docker Daemon is listening for TLS traffic"
+}
+
+variable "aws-region" {
+  type        = "string"
+  description = "The AWS region to place the infrastructure in"
+}
+
+variable "nr-assign-elastic-ip" {
+  type        = "string"
+  description = "Whether to assign an elastic IP to the instance"
+}
+
+variable "nr-aws-tls-cert-domain" {
+  type        = "string"
+  description = "Domain name of a TLS certificate in AWS Certificate Manager. It is probably *.nr-route53-zone-name if that is set."
+}
+
+variable "nr-elb-subnets" {
+  type        = "list"
+  description = "Subnets to attach to the ELB"
+}
+
+variable "nr-use-route53-domain" {
+  type        = "string"
+  description = "Whether to use route53 hosted domain"
+  default     = false
+}
+
+variable "nr-route53-zone-name" {
+  type        = "string"
+  description = "The route53 hosted zone name to use"
+  default     = ""
+}
+
+variable "nr-nifi-domain" {
+  type        = "string"
+  description = "The nifi domain to create. Should be a subdomain of nr-route53-zone-name"
+  default     = ""
+}
+
+variable "nr-superset-domain" {
+  type        = "string"
+  description = "The superset domain to create. Should be a subdomain of nr-route53-zone-name"
+  default     = ""
+}
