e04e6e32dec7b90c33fdf427a3efdc22bbf5def9
Łukasz Lewczyński <lukaslew@users.noreply.github.com>
2019-08-19 11:12:13 +0200

    OLMIS-6478: Moved env variables to settings file

--- deployment/reporting_env/services/deploy_services.sh
+++ deployment/reporting_env/services/deploy_services.sh
@@ -6,15 +6,6 @@ export DOCKER_HOST="tcp://nifi-registry.openlmis.org:2376"
 export DOCKER_COMPOSE_BIN=/usr/local/bin/docker-compose
 export REPORTING_DIR_NAME=reporting
 
-export SUPERSET_ENABLE_SSL=false
-export SUPERSET_BEHIND_LOAD_BALANCER=true
-export SUPERSET_LOAD_BALANCER_REDIRECT_HTTP=true
-export SUPERSET_DOMAIN_NAME=superset.uat.openlmis.org
-export NIFI_ENABLE_SSL=false
-export NIFI_BEHIND_LOAD_BALANCER=true
-export NIFI_LOAD_BALANCER_REDIRECT_HTTP=true
-export NIFI_DOMAIN_NAME=nifi.uat.openlmis.org
-
 reportingRepo=$1
 
 cd "$reportingRepo/$REPORTING_DIR_NAME"
