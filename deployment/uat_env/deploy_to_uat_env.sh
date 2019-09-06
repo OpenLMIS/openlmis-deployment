b1b09903fdefe049b449a6236c4473087c79cb7f
Sebastian Brudziński <sbrudzinski@soldevelo.com>
2019-09-06 08:40:12 +0200

    Checking if No-TLS-Verify fixes the build temporarily

--- deployment/uat_env/deploy_to_uat_env.sh
+++ deployment/uat_env/deploy_to_uat_env.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-export DOCKER_TLS_VERIFY="1"
+export DOCKER_TLS_VERIFY="0"
 export DOCKER_HOST="tcp://uat-env-elb-1284865908.us-east-1.elb.amazonaws.com:2376"
 export DOCKER_CERT_PATH="${PWD}/credentials"
 
