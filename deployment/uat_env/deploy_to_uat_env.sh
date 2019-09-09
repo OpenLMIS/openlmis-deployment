b6244f224b3341ed8c772d4700b33f874b6d2776
Sebastian Brudziński <sbrudzinski@soldevelo.com>
2019-09-09 10:17:30 +0200

    Try bringing back TLS Verify

--- deployment/uat_env/deploy_to_uat_env.sh
+++ deployment/uat_env/deploy_to_uat_env.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-export DOCKER_TLS_VERIFY="0"
+export DOCKER_TLS_VERIFY="1"
 export DOCKER_HOST="tcp://uat1-env-elb-869202823.us-east-1.elb.amazonaws.com:2376"
 export DOCKER_CERT_PATH="${PWD}/credentials"
 
