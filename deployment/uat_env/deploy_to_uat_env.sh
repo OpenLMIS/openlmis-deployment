dd9f3183a4b1b35e0630b3fcc7606623c9dfb0f1
Sebastian Brudziński <sbrudzinski@soldevelo.com>
2019-09-10 09:27:26 +0200

    Bring back TLS verify

--- deployment/uat_env/deploy_to_uat_env.sh
+++ deployment/uat_env/deploy_to_uat_env.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-export DOCKER_TLS_VERIFY="0"
-export DOCKER_HOST="tcp://uat1-env-elb-869202823.us-east-1.elb.amazonaws.com:2376"
+export DOCKER_TLS_VERIFY="1"
+export DOCKER_HOST="tcp://uat.openlmis.org:2376"
 export DOCKER_CERT_PATH="${PWD}/credentials"
 
 ../shared/init_env_gh.sh
