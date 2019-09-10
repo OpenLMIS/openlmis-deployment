9c0b7a2df6acf5573f220f4b728d632f43ab47ac
Sebastian Brudziński <sbrudzinski@soldevelo.com>
2019-09-10 16:13:48 +0200

    Try going with ELB as docker host

--- deployment/test_env/deploy_to_test_env.sh
+++ deployment/test_env/deploy_to_test_env.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 export DOCKER_TLS_VERIFY="1"
-export DOCKER_HOST="tcp://test.openlmis.org:2376"
+export DOCKER_HOST="tcp://test-env-elb-804763558.us-east-1.elb.amazonaws.com:2376"
 export DOCKER_CERT_PATH="${PWD}/credentials"
 
 ../shared/init_env_gh.sh
