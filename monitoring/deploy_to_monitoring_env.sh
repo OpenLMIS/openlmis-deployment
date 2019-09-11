1cb4caa951b7955a10ba1e594d66f9ea61722146
pgarrison <philipmg@cs.washington.edu>
2019-09-11 13:45:43 -0700

    OLMIS-6555 Use Jenkins's new docker secret format

--- monitoring/deploy_to_monitoring_env.sh
+++ monitoring/deploy_to_monitoring_env.sh
@@ -2,7 +2,6 @@
 
 export DOCKER_TLS_VERIFY="1"
 export DOCKER_HOST="grafana.a.openlmis.org:2376"
-export DOCKER_CERT_PATH="${credentials}"
 
 /usr/local/bin/docker-compose kill
 /usr/local/bin/docker-compose down -v
