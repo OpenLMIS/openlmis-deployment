2805df277a4f895f8017edd9d676a96642879e1d
pgarrison <philipmg@cs.washington.edu>
2019-09-18 16:01:55 -0700

    OLMIS-6555 Add Prometheus and scrape from Jenkins
    Also updates our dashboard with one metric from Jenkins, the number of
    plugin updates available

--- monitoring/deploy_to_monitoring_env.sh
+++ monitoring/deploy_to_monitoring_env.sh
@@ -6,8 +6,10 @@ export DOCKER_HOST="grafana.a.openlmis.org:2376"
 /usr/local/bin/docker-compose kill
 if [ "$KEEP_OR_WIPE" == "WIPE" ]; then
     /usr/bin/docker volume rm influxdb-volume
+    /usr/bin/docker volume rm prometheus-volume
 fi
 /usr/local/bin/docker-compose down -v
 /usr/bin/docker volume create influxdb-volume
+/usr/bin/docker volume create prometheus-volume
 /usr/local/bin/docker-compose pull
 /usr/local/bin/docker-compose up --build --force-recreate -d
