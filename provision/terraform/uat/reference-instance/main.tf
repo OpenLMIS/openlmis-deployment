f7fac2ec2a05b9b713ffc32b6884223cd83dda4b
Klaudia Pałkowska <kpalkowska@soldevelo.com>
2020-04-17 10:10:06 +0200

    COV-15: Added terraform configuration for reference_instance

---
+++ provision/terraform/uat/reference-instance/main.tf
@@ -0,0 +1,27 @@
+terraform {
+  backend "s3" {
+    "bucket" = "openlmis-terraform-states"
+    "key"    = "reference-instance.tf"
+    "region" = "us-east-1"
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+module "reference-instance" {
+  source = "../../modules/reference-instance"
+
+  name                         = "${var.name}"
+  app-instance-ssh-user        = "${var.app-instance-ssh-user}"
+  docker-ansible-dir           = "${var.docker-ansible-dir}"
+  docker-tls-port              = "${var.docker-tls-port}"
+  app-tls-s3-access-key-id     = "${var.aws_access_key_id}"
+  app-tls-s3-secret-access-key = "${var.aws_secret_access_key}"
+  app-dns-name                 = "${var.app-dns-name}"
+  app-instance-group           = "${var.app-instance-group}"
+  olmis-db-instance-class      = "${var.olmis-db-instance-class}"
+  olmis-db-username            = "${var.olmis-db-username}"
+  olmis-db-password            = "${var.olmis-db-password}"
+}
---
+++ provision/terraform/uat/reference-instance/terraform.tfvars
@@ -0,0 +1,15 @@
+name = "reference-instance"
+
+app-instance-ssh-user = "ubuntu"
+
+docker-ansible-dir = "../../../ansible/"
+
+docker-tls-port = "2376"
+
+app-dns-name = "stayathome.openlmis.org"
+
+app-instance-group = "docker-hosts"
+
+olmis-db-instance-class = "db.t3.medium"
+olmis-db-username="aUGYxxtqoGGimxKs"
+olmis-db-password="TZYbkZbVnVkodWBX"
