ed655c9cbfe90e53670bee42f63bd7309d20a20d
oskarhinc <oskarhinc@gmail.com>
2020-05-26 09:46:50 +0200

    COV-73: Changed the domain for the reporting stack

--- deployment/covid-reporting_env/nifi-registry/deploy_nifi_registry.sh
+++ deployment/covid-reporting_env/nifi-registry/deploy_nifi_registry.sh
@@ -3,7 +3,7 @@
 set -e
 export DOCKER_TLS_VERIFY="1"
 export COMPOSE_TLS_VERSION=TLSv1_2
-export DOCKER_HOST="tcp://report.covid-ref.openlmis.org:2376"
+export DOCKER_HOST="tcp://covid-ref-report.openlmis.org:2376"
 export DOCKER_CERT_PATH="${PWD}/../../../credentials/covid-reporting"
 
 /usr/local/bin/docker-compose kill
--- deployment/covid-reporting_env/services/deploy_services.sh
+++ deployment/covid-reporting_env/services/deploy_services.sh
@@ -2,7 +2,7 @@
 
 export DOCKER_TLS_VERIFY="1"
 export COMPOSE_TLS_VERSION=TLSv1_2
-export DOCKER_HOST="tcp://report.covid-ref.openlmis.org:2376"
+export DOCKER_HOST="tcp://covid-ref-report.openlmis.org:2376"
 export DOCKER_COMPOSE_BIN=/usr/local/bin/docker-compose
 export DOCKER_CERT_PATH="${PWD}/../../../credentials/covid-reporting"
 export REPORTING_DIR_NAME=covid-reporting
