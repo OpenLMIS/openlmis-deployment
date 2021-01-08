796ea39b8c6fbe7f0ac4f4a025a171a83d35f021
Klaudia Pałkowska <kpalkowska@soldevelo.com>
2021-01-08 13:16:49 +0100

    OLMIS-7174: Fixed deploy script for uat

--- deployment/functional_test_env/deploy_to_env.sh
+++ deployment/functional_test_env/deploy_to_env.sh
@@ -14,17 +14,12 @@ export DOCKER_CERT_PATH="${PWD}/credentials"
 ../shared/init_env_gh.sh
 
 if [ ! -z "$ENV_RESTORE_SNAPSHOT" ]; then
-  export KEEP_OR_WIPE="use_env"
   cp .deployment-config/$ENV_RESTORE_SNAPSHOT settings.env
   docker pull openlmis/restore-snapshot
   docker pull openlmis/obscure-data
   /usr/bin/docker run --rm --env-file settings.env openlmis/restore-snapshot
   sleep 300
   /usr/bin/docker run --rm --env-file settings.env openlmis/obscure-data
-else
-  cp .deployment-config/functional-test.env settings.env
-  docker pull openlmis/demo-data
-  /usr/bin/docker run --rm --env-file settings.env openlmis/demo-data
 fi
 
 ../shared/pull_images.sh $1
