a31cf854108c2b5295ad7c776e0d67950082985a
Klaudia Pałkowska <kpalkowska@soldevelo.com>
2021-01-12 13:26:20 +0100

    Revert "OLMIS-7174: Added else statement to the deploy script for functional test"
    This reverts commit 284b864f190d5134094d8c503d87eea2587a26e9.

--- deployment/functional_test_env/deploy_to_env.sh
+++ deployment/functional_test_env/deploy_to_env.sh
@@ -20,10 +20,6 @@ if [ ! -z "$ENV_RESTORE_SNAPSHOT" ]; then
   /usr/bin/docker run --rm --env-file settings.env openlmis/restore-snapshot
   sleep 300
   /usr/bin/docker run --rm --env-file settings.env openlmis/obscure-data
-else
-  cp .deployment-config/functional-test.env settings.env
-  docker pull openlmis/demo-data
-  /usr/bin/docker run --rm --env-file settings.env openlmis/demo-data
 fi
 
 ../shared/pull_images.sh $1
