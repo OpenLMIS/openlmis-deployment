d142c71fa6c87aed94a72da6f3261247a5a8214b
Klaudia Pałkowska <kpalkowska@soldevelo.com>
2021-01-18 16:17:41 +0100

    OLMIS-7185: Temporarily changed docker host to ELB

--- deployment/demo-drc_env/deploy_to_demo_env.sh
+++ deployment/demo-drc_env/deploy_to_demo_env.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 export DOCKER_TLS_VERIFY="1"
-export DOCKER_HOST="tcp://demo-drc.openlmis.org:2376"
+export DOCKER_HOST="tcp://demo-drc-env-elb-1549601289.us-east-1.elb.amazonaws.com:2376"
 export DOCKER_CERT_PATH="${PWD}/credentials"
 
 ../shared/init_env_gh.sh
