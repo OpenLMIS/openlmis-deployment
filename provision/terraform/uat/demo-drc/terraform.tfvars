1597c84027544163c4ee364b670c61981aa349da
Klaudia Pałkowska <kpalkowska@soldevelo.com>
2021-01-18 15:24:19 +0100

    OLMIS-7185: Added demo instance for DRC

---
+++ provision/terraform/uat/demo-drc/terraform.tfvars
@@ -0,0 +1,15 @@
+name = "demo-drc"
+
+app-instance-ssh-user = "ubuntu"
+
+docker-ansible-dir = "../../../ansible/"
+
+docker-tls-port = "2376"
+
+app-dns-name = "demo-drc.openlmis.org"
+
+app-instance-group = "docker-hosts"
+
+olmis-db-instance-class = "db.t2.medium"
+olmis-db-username="pHvNEhePnLpdATZe"
+olmis-db-password="rTOGfwiHrJkNhXYI"
---
+++ provision/terraform/uat/demo-drc/variables.tf
@@ -0,0 +1,54 @@
+variable "name" {
+  type        = "string"
+  description = "Name of the environment"
+}
+
+variable "app-instance-ssh-user" {
+  type        = "string"
+  description = "The name of the user to connect as in the app host"
+}
+
+variable "docker-ansible-dir" {
+  type        = "string"
+  description = "The path to the directory containing the playbook for installing docker"
+}
+
+variable "docker-tls-port" {
+  type        = "string"
+  description = "The TCP port The Docker Daemon is listening for TLS traffic"
+}
+
+variable "app-dns-name" {
+  type        = "string"
+  description = "The DNS name associated to the app instance"
+}
+
+variable "app-instance-group" {
+  type        = "string"
+  description = "The deployment group to place the app host. Will dictate what Ansible playbooks can ran on it"
+}
+
+variable "olmis-db-instance-class" {
+  type        = "string"
+  description = "The RDS db instance type. If left empty then RDS instance will not be used."
+}
+
+variable "olmis-db-username" {
+  type        = "string"
+  description = "PostgreSQL database username."
+}
+
+variable "olmis-db-password" {
+  type        = "string"
+  description = "The PostgreSQL database password."
+}
+
+variable "aws_access_key_id" {
+  type        = "string"
+  description = "The AWS access key ID to use to backup generated Docker TLS files"
+}
+
+variable "aws_secret_access_key" {
+  type        = "string"
+  description = "The AWS secrect access key to use to backup generated Docker TLS files"
+}
