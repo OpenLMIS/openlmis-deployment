875309eebc863cb710648ae2f1e525e5f9a2111e
saleksandra <soltys.ola@gmail.com>
2021-02-03 13:54:30 +0100

    OLMIS-7163: Removed condition with ENV_RESTORE_SNAPSHOT param from deploy script in functional-tests

--- deployment/functional_test_env/deploy_to_env.sh
+++ deployment/functional_test_env/deploy_to_env.sh
@@ -13,15 +13,6 @@ export DOCKER_CERT_PATH="${PWD}/credentials"
 
 ../shared/init_env_gh.sh
 
-if [ ! -z "$ENV_RESTORE_SNAPSHOT" ]; then
-  cp .deployment-config/$ENV_RESTORE_SNAPSHOT settings.env
-  docker pull openlmis/restore-snapshot
-  docker pull openlmis/obscure-data
-  /usr/bin/docker run --rm --env-file settings.env openlmis/restore-snapshot
-  sleep 300
-  /usr/bin/docker run --rm --env-file settings.env openlmis/obscure-data
-fi
-
 ../shared/pull_images.sh $1
 
 ../shared/restart.sh $1
