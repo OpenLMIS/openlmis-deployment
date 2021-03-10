6eaedbef09af05a91077d753f3896cc6eccc4c36
saleksandra <soltys.ola@gmail.com>
2021-03-10 13:25:53 +0100

    OLMIS-6856: Added pgdata volume in demo-v3

--- deployment/demo_env/deploy_to_demo_env.sh
+++ deployment/demo_env/deploy_to_demo_env.sh
@@ -4,6 +4,13 @@ export DOCKER_TLS_VERIFY="1"
 export DOCKER_HOST="demo-v3.openlmis.org:2376"
 export DOCKER_CERT_PATH="${PWD}/credentials"
 
+if [ "$KEEP_OR_WIPE" == "wipe" ]; then
+  echo "Removing pgdata volume"
+  docker volume rm pgdata
+fi
+echo "Creating pgdata volume"
+docker volume create pgdata
+
 ../shared/init_env_gh.sh
 
 ../shared/pull_images.sh $1
