481ac7c264e299e7aafccfffacbf3204ced0e4b1
hdsldv <88383112+hdsldv@users.noreply.github.com>
2021-12-28 17:22:27 +0100

    OLMIS-7472 Terraform modules use postgres 12.8 storage

--- provision/terraform/modules/openlmis/storage.tf
+++ provision/terraform/modules/openlmis/storage.tf
@@ -5,7 +5,7 @@ resource "aws_db_instance" "rds" {
   allocated_storage      = 10
   storage_type           = "gp2"
   engine                 = "postgres"
-  engine_version         = "9.6.23"
+  engine_version         = "12.8"
   allow_major_version_upgrade = true
   instance_class         = "${var.olmis-db-instance-class}"
   name                   = "open_lmis"
