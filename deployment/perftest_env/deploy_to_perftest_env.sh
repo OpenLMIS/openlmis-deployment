a55f9e9b34e1d0ccfc43d1e77ca200f4ee5a836d
Paweł Gesek <pgesek@soldevelo.com>
2022-09-28 09:05:17 +0200

    Update Perftest URL

--- deployment/perftest_env/deploy_to_perftest_env.sh
+++ deployment/perftest_env/deploy_to_perftest_env.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 export DOCKER_TLS_VERIFY="1"
-export DOCKER_HOST="tcp://perftest-env-elb-2066004734.us-east-1.elb.amazonaws.com:2376"
+export DOCKER_HOST="tcp://perftest.openlmis.org:2376"
 export DOCKER_CERT_PATH="${PWD}/credentials"
 
 ../shared/init_env_gh.sh
