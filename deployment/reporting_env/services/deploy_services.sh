4c54783b9e8fc0851225a177065c8bcdb629ae98
Paweł Gesek <pgesek@soldevelo.com>
2023-02-13 18:13:59 +0100

    Fix cert path

--- deployment/reporting_env/services/deploy_services.sh
+++ deployment/reporting_env/services/deploy_services.sh
@@ -4,7 +4,7 @@ export DOCKER_TLS_VERIFY="1"
 export COMPOSE_TLS_VERSION=TLSv1_2
 export DOCKER_HOST="tcp://superset-uat.openlmis.org:2376"
 export DOCKER_COMPOSE_BIN=/usr/local/bin/docker-compose
-export DOCKER_CERT_PATH="${PWD}/.deployment-config/reporting-uat-certs"
+export DOCKER_CERT_PATH="${PWD}/../../../.deployment-config/reporting-uat-certs"
 export REPORTING_DIR_NAME=reporting
 
 ../../shared/init_env_gh.sh
