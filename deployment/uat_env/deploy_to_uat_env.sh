a6d67ab5d1764478509849d3ac3aaddc72b88434
Mateusz Sobolak <msobolak@soldevelo.com>
2023-09-21 15:35:26 +0200

    Update deploy_to_uat_env.sh

--- deployment/uat_env/deploy_to_uat_env.sh
+++ deployment/uat_env/deploy_to_uat_env.sh
@@ -2,7 +2,7 @@
 
 export DOCKER_TLS_VERIFY="1"
 export DOCKER_HOST="tcp://uat.openlmis.org:2376"
-export DOCKER_CERT_PATH="${PWD}/credentials"
+export DOCKER_CERT_PATH="${PWD}/.deployment-config/uat-certs"
 
 ../shared/init_env_gh.sh
 
