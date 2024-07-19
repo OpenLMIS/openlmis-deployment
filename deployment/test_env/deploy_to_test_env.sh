e151b4b823732448129a4eb6ce0bc7c279eadfde
Paweł Gesek <pgesek@soldevelo.com>
2024-07-19 12:19:01 +0200

    Update test deployment script

--- deployment/test_env/deploy_to_test_env.sh
+++ deployment/test_env/deploy_to_test_env.sh
@@ -2,7 +2,7 @@
 
 export DOCKER_TLS_VERIFY="1"
 export DOCKER_HOST="tcp://test.openlmis.org:2376"
-export DOCKER_CERT_PATH="${PWD}/credentials"
+export DOCKER_CERT_PATH="${PWD}/credentials/test-certs"
 
 ../shared/init_env_gh.sh
 
